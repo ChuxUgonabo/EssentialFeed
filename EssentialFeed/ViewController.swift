69b72c2a5a3ed9d952baf4d539d9c87cead1dd86
John_Ugonabo <chuxugonabo@gmail.com>
2023-03-03 13:59:32 -0800

    Initial Commit

---
+++ EssentialFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialFeed
+//
+//  Created by Chux Ugonabo MacBook on 2023-03-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
